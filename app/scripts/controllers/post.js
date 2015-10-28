ecf63ed040f41aeec3699a300f486064b99477d1
Gunendu Das <gunendudas@Gunendu.local>
2015-10-29 01:45:25 +0530

    image upload changes

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -1,8 +1,10 @@
 'use strict';
 
-angular.module('phApp.PostView',['ngRoute'])
-  .controller('PostCtrl', function($scope,apiservice) {
-    $scope.savePost = function () {
+var myapp = angular.module('phApp.PostView',['ngRoute'])
+
+myapp.controller('PostCtrl', function($scope,$http,apiservice) {
+    $scope.savePost = function (files) {
+      console.log("files is",files[0],files[1]);
       var post = {};
       post.title = $scope.Title;
       post.url = $scope.Url;
@@ -11,4 +13,35 @@ angular.module('phApp.PostView',['ngRoute'])
         console.log("post is",response);
       });
     };
-  });
+
+    $scope.uploadFile = function(files) {
+        console.log(files);
+        var fd = new FormData();
+        fd.append("file", files[0],files[1]);
+
+        $http.post('http://localhost:9005/user/image', fd, {
+            withCredentials: true,
+            headers: {'Content-Type': undefined },
+            transformRequest: angular.identity
+        })
+    };
+});
+
+myapp.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
+}]);
