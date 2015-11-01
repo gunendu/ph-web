2115831be1514a2497a688baa5476e253216b7a1
Gunendu Das <gunendudas@Gunendu.local>
2015-11-01 19:24:02 +0530

    post details controller changes

--- app/scripts/controllers/postdetails.js
+++ app/scripts/controllers/postdetails.js
@@ -1,15 +1,15 @@
 'use strict';
 
-angular.module('phApp.PostViewDetails',['ngRoute'])
-  .controller('PostDetailsCtrl',function ($scope,$routeParams,$cookies,apiservice) {
+angular.module('phApp.PostViewDetails',['ngRoute','ngStorage'])
+  .controller('PostDetailsCtrl',function ($scope,$routeParams,$localStorage,apiservice) {
   $scope.post_id = $routeParams.post_id;
   $scope.sendComment = function(message) {
     console.log("message is",message,$scope.post_id);
     var comment = {};
     comment.post_id = $scope.post_id;
     comment.comment = message;
-    comment.user_id = $cookies.get('user_id');
-    var username = $cookies.get('username');
+    comment.user_id = $localStorage.user_id;
+    var username = $localStorage.username;
     console.log("comment details",comment,username);
     apiservice.comment.save(comment,function(response) {
       console.log("comment save response",response);
